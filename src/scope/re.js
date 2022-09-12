ebde817244c510e79a72e003f6f1c5e09e468326
sntwandy <sntwandy@gmail.com>
2022-09-12 00:09:59 -0400

    feat: ✨Understanding the reassigment, declaration, initialization and reinitialization etc

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var firstName // undefined
+firstName = 'Megan'
+console.log(firstName)
+
+var lastName = 'Herrera' // declaration and initialization
+lastName = 'Santana' // reassignment
+console.log(lastName)
+
+var secondName = 'Estefanie' // declaration and initialization
+var secondName = 'Gravity' // redeclaration and reinitialization
+console.log(secondName)
+
+// let
+let fruit = 'Pineapple' // declaration and initialization
+fruit = 'Banana' // reassignment
+console.log(fruit)
+
+let fruit = 'Kiwi'
+console.log(fruit)
+
+// const
+const animal = 'Dog' // declaration and initialization
+animal = 'Cat' // reassignment
+const animal = 'Bird'
+console.log(animal)
+
+const vehicles = []
+vehicles.push('Car')
+console.log(vehicles)
+
+vehicles.pop()
+console.log(vehicles)
