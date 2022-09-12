e891b4117fa33a269250dc793020dc6f4899f559
sntwandy <sntwandy@gmail.com>
2022-09-11 23:43:02 -0400

    feat: ✨Understanding the function scope

---
+++ src/scope/function.js
@@ -0,0 +1,13 @@
+function greeting() {
+  let userName = 'Megan'
+  console.log(userName)
+
+  if (userName === 'Megan') {
+    console.log(`Hello ${userName}!`)
+  }
+
+}
+
+greeting()
+
+console.log(userName)
