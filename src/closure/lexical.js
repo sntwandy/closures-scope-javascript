fea751fbee27c1f757a0bcbf2168e2a2e0fd20cd
sntwandy <sntwandy@gmail.com>
2022-09-19 04:39:38 -0400

    feat: ✨Understanding closures

---
+++ src/closure/lexical.js
@@ -0,0 +1,17 @@
+const myGlobal = 0
+
+function getGlobal() {
+  const myNumber = 1
+  console.log(myGlobal)
+
+  function parent() { // parent() is a closure
+    const inner = 2
+    console.log(myNumber, myGlobal)
+
+    function child() { // child() is a closure
+      console.log(inner, myNumber, myGlobal)
+    }
+    return child()
+  }
+  return parent()
+}
