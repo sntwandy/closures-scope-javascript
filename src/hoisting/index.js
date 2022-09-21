506aa97af70efab8a2ed9ea2e4295146318e9d5c
sntwandy <sntwandy@gmail.com>
2022-09-20 23:19:24 -0400

    feat: ✨Understanding hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+// var nameOfDog // undefined
+
+// console.log(nameOfDog)
+// var nameOfDog = 'Elmo'
+// console.log(nameOfDog)
+
+nameOfDog()
+
+function nameOfDog() {
+  console.log(`The dog is ${elmo}`)
+}
+
+var elmo = 'Elmito'
